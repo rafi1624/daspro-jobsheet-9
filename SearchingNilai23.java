41099acc9aae169403314a683bae938fac9888f3
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-10-31 12:57:53 +0700

    Percobaan 4 dan Jawaban pertanyaan

--- SearchingNilai23.java
+++ SearchingNilai23.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class SearchingNilai23 {
 
     public static void main(String[] args) {
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
-
-        for (int i = 0; i < arrNilai.length; i++){
-            if (key == arrNilai[i]) {
-                hasil = i;
-                break;
-            }
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = scanner.nextInt();
+        int[] arrNilai = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = scanner.nextInt();
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " Ketemu Di Indeks Ke- " + hasil);
-        System.out.println();
+        
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = scanner.nextInt();
+        int hasil = -1; 
+
+       
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
+                hasil = i + 1;
+                break; 
+            }
+        }       
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam daftar.");
+        }
     }
 }
