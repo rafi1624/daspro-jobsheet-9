c7daba7581318588b9c033ab60a19816d490f56f
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-10-31 11:24:42 +0700

    Modifikasi dari pertanyaan percobaan 2

--- ArrayNilai23.java
+++ ArrayNilai23.java
@@ -6,12 +6,14 @@ public class ArrayNilai23 {
         Scanner sc = new Scanner(System.in);
         int [] nilaiAkhir = new int[10];
 
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < nilaiAkhir.length; i++){
         System.out.print("Masukkan Nilai Akhir Ke-" + i + " : ");
         nilaiAkhir[i] = sc.nextInt();
     }
-    for (int i = 0; i < 10; i++){
-        System.out.println("Nilai Akhir Ke-"+ i + " Adalah " + nilaiAkhir[i]);
+    for (int i = 0; i < nilaiAkhir.length; i++){
+        if (nilaiAkhir[i] > 70) {
+            System.out.println("Mahasiswa Ke-" + i + " Lulus ");
+        }
     }
 
     } 
